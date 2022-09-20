aea10475fc6503637630676dd6d155813c47ea31
Seymur <ibragimovs16@mail.ru>
2022-09-20 21:46:07 +0300

    Configured Program.cs

--- Otium/Otium/Program.cs
+++ Otium/Otium/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Otium.Domain.Models.Settings;
 using Otium.Repositories;
@@ -55,6 +56,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
