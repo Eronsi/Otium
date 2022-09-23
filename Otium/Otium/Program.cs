c1414ce5bd0cc050daf0b25863cb80371a33716c
Seymur <ibragimovs16@mail.ru>
2022-09-23 05:05:14 +0300

    ...

--- Otium/Otium/Program.cs
+++ Otium/Otium/Program.cs
@@ -51,7 +51,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
