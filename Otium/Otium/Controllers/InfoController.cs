aa754cabcd6a23a7eacd4c6e867ae7e2937cada6
Seymur <ibragimovs16@mail.ru>
2022-09-28 16:13:23 +0300

    Moved the contact information to appsettings.json and implemented its transmission.

--- Otium/Otium/Controllers/InfoController.cs
+++ Otium/Otium/Controllers/InfoController.cs
@@ -7,9 +7,13 @@ namespace Otium.Controllers;
 public class InfoController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public InfoController(ILogger<HomeController> logger) => 
+    public InfoController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
         _logger = logger;
+        _configuration = configuration;
+    }
 
     [HttpGet]
     public IActionResult About() => View();
@@ -24,7 +28,19 @@ public class InfoController : Controller
     public IActionResult Payment() => View();
 
     [HttpGet]
-    public IActionResult Contact() => View();
+    public IActionResult Contact()
+    {
+        var viewModel = new ContactsViewModel
+        {
+            Phone1 = _configuration.GetSection("Contacts:Phone1").Value,
+            Tg1 = _configuration.GetSection("Contacts:Tg1").Value,
+            Phone2 = _configuration.GetSection("Contacts:Phone2").Value,
+            Tg2 = _configuration.GetSection("Contacts:Tg2").Value,
+            AdminEmail = _configuration.GetSection("Contacts:AdminEmail").Value
+        };
+        
+        return View(viewModel);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Otium/Otium/Models/ContactsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Otium.Models;
+
+public class ContactsViewModel
+{
+    public string Phone1 { get; set; } = string.Empty;
+    public string Tg1 { get; set; } = string.Empty;
+    public string Phone2 { get; set; } = string.Empty;
+    public string Tg2 { get; set; } = string.Empty;
+    public string AdminEmail { get; set; } = string.Empty;
+}
